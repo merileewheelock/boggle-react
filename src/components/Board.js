06878992dd4d729e9e95b66c43d2e66989721faf
Merilee Wheelock <eelirem@gmail.com>
2017-11-09 15:44:33 -0800

    Letter build now working. Need to finish score board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Dice from './Dice';
+import $ from 'jquery';
 
 var dice = new Dice()
 
@@ -11,9 +12,12 @@ class Board extends Component{
 			currentWord: [],
 			displayWord: '',
 			submittedWords: [],
-			scoreArray: []
+			scoreArray: [],
+			selectedIndexArray: []
 		}
+		this.clickLetter = this.clickLetter.bind(this);
 		this.buildWord = this.buildWord.bind(this);
+		// this.checkAdjacentLetters = this.checkAdjacentLetters.bind(this);
 	}
 
 	componentDidMount(){
@@ -25,13 +29,103 @@ class Board extends Component{
 		})
 	}
 
+	clickLetter(value, index){
+
+		// Had to create lettersList to access the classes in each dice div
+		var lettersList = $('.dice');
+
+		if ($(lettersList[index]).hasClass('current')){
+			$(lettersList[index]).removeClass('selected');
+			$(lettersList[index]).removeClass('current');
+
+			this.state.currentWord.pop();
+			this.state.selectedIndexArray.pop();
+
+			var previousLetterIndex = this.state.selectedIndexArray[this.state.selectedIndexArray.length-1];
+			this.checkAdjacentLetters(previousLetterIndex, lettersList);
+			$(lettersList[previousLetterIndex]).addClass('current');
+			
+			// Here instead of running buildWord(), which adds to the word, we are deleting from the word
+			var tempCurrentWord = this.state.currentWord.join("");
+			this.setState({
+				displayWord: tempCurrentWord
+			})
+
+			// Allows user to keep playing after completely deleting the word
+			if (this.state.currentWord.length === 0){
+				$('.dice').removeClass('letter-off');
+				// console.log("empty")
+			}
+			return
+		}if ($(lettersList[index]).hasClass('letter-off')){
+			// console.log("letter is not adjacent, cannot click")
+			return
+		}if ($(lettersList[index]).hasClass('selected')){
+			// console.log("letter has already been selected, cannot click")
+			return
+		}
+		this.buildWord(value)
+		$('.current').removeClass('current') // Updates the current letter
+		this.state.selectedIndexArray.push(index);
+		this.checkAdjacentLetters(index, lettersList);
+		$(lettersList[index]).addClass('selected');
+		$(lettersList[index]).addClass('current');
+	}
+
 	buildWord(value){
-		// console.log(value)
+		// This actually builds the word that is displayed on the screen
 		this.state.currentWord.push(value);
 		var displayWord = this.state.currentWord.join('');
 		this.setState({
 			displayWord: displayWord
 		})
+	}
+
+	checkAdjacentLetters(index, lettersList){
+		// console.log("current index: " + index)
+		// console.log(lettersList)
+
+		for (let i = 0; i < lettersList.length; i++){
+			$(lettersList[i]).addClass('letter-off');
+			$(lettersList[index]).removeClass('letter-off');
+		}
+
+		// Y Axis
+		if (index <= 4){
+			$(lettersList[index + 5]).removeClass('letter-off');
+		}if ((index >= 5) && (index <= 19)){
+			$(lettersList[index + 5]).removeClass('letter-off');
+			$(lettersList[index - 5]).removeClass('letter-off');
+		}if (index >= 20){
+			$(lettersList[index - 5]).removeClass('letter-off');
+		}
+
+		// X Axis
+		if ((index === 0) || (index === 5) || (index === 10) || (index === 15) || (index === 20)){
+			$(lettersList[index + 1]).removeClass('letter-off');
+		}if ((index === 1) || (index === 6) || (index === 11) || (index === 16) || (index === 21)){
+			$(lettersList[index + 1]).removeClass('letter-off');
+			$(lettersList[index - 1]).removeClass('letter-off');
+		}if ((index === 2) || (index === 7) || (index === 12) || (index === 17) || (index === 22)){
+			$(lettersList[index + 1]).removeClass('letter-off');
+			$(lettersList[index - 1]).removeClass('letter-off');
+		}if ((index === 3) || (index === 8) || (index === 13) || (index === 18) || (index === 23)){
+			$(lettersList[index + 1]).removeClass('letter-off');
+			$(lettersList[index - 1]).removeClass('letter-off');
+		}if ((index === 4) || (index === 9) || (index === 14) || (index === 19) || (index === 24)){
+			$(lettersList[index - 1]).removeClass('letter-off');
+		}
+
+		// Diagonals
+		if ((index !== 4) && (index !== 9) && (index !== 14) && (index <= 19)){
+			$(lettersList[index + 6]).removeClass('letter-off');
+		}if ((index !== 0) && (index !== 5) && (index !== 10) && (index !== 15) && (index < 20)){
+			$(lettersList[index + 4]).removeClass('letter-off');
+		}if ((index > 5) && (index !== 10) && (index !== 15) && (index !== 20)){
+			$(lettersList[index - 6]).removeClass('letter-off');
+		}if ((index > 4) && (index !== 9) && (index !== 14) && (index !== 19) && (index !== 24)){
+			$(lettersList[index - 4]).removeClass('letter-off');
+		}
 	}
 
 	clickSubmit(){
@@ -65,6 +159,11 @@ class Board extends Component{
 			currentWord: [],
 			displayWord: ''
 		})
+
+		// Resets board
+		$('.dice').removeClass('selected');
+		$('.dice').removeClass('current');
+		$('.dice').removeClass('letter-off');
 	}
 
 	
@@ -80,12 +179,11 @@ class Board extends Component{
 			}
 
 			diceOnBoard.push(
-				<div className="dice" onClick={()=>this.buildWord(die)} key={index}>
+				<div className="dice" onClick={()=>this.clickLetter(die, index)} key={index}>
 					{die}
 				</div>
 			)
 		})
-
 
 		var submittedWordsHTML = [];
 		this.state.submittedWords.map((word, index)=>{
